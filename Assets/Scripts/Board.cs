af76d0f0483a9703b3ec3d79b228983287116f98
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-11 23:18:49 -0300

    feat: implement setup camera

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    public int width, height;
-    public GameObject tilePrefab;
-
     private Tile[,] allTiles;
+
+    public int width, height, borderSize;
+    public GameObject tilePrefab;
 
     private void Start()
     {
         this.allTiles = new Tile[width, height];
         SetupTiles();
+        SetupCamera();
     }
 
     private void SetupTiles()
     {
-        for (int i = 0; i < width; i++)
+        for (int i = 0; i < this.width; i++)
         {
-            for (int j = 0; j < height; j++)
+            for (int j = 0; j < this.height; j++)
             {
-                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+                GameObject tile = Instantiate(this.tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
                 tile.name = $"Tile ({i},{j})";
                 this.allTiles[i, j] = tile.GetComponent<Tile>();
                 tile.transform.parent = this.transform;
             }
         }
+    }
+
+    private void SetupCamera()
+    {
+        Camera.main.transform.position = new Vector3((float)(this.width - 1)/2f, (float)(this.height - 1)/ 2f, -10f);
+
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        float verticalSize = (float)this.height/2f * (float)this.borderSize;
+        float horizontalSize = ((float)this.width/2f + (float)this.borderSize) / aspectRatio;
+
+        Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
     }
 }
