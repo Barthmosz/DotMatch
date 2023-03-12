237f9a4088b19b74cad4c8a5176c0960c4646c8d
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-12 14:40:07 -0300

    feat: create base mouse input methods

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GamePiece : MonoBehaviour
 {
     private bool isMoving = false;
+    private Tile clickedTile;
+    private Tile targetTile;
+
     public int xIndex, yIndex;
 
     public enum InterpolationType
@@ -86,5 +89,36 @@ public class GamePiece : MonoBehaviour
         }
 
         this.isMoving = false;
+    }
+
+    public void ClickTile(Tile tile)
+    {
+        if (this.clickedTile == null)
+        {
+            this.clickedTile = tile;
+        }
+    }
+
+    public void DragToTile(Tile tile)
+    {
+        if (this.clickedTile != null)
+        {
+            this.targetTile = tile;
+            Debug.Log($"Clicked tile: {tile.name}");
+        }
+    }
+
+    public void ReleaseTile()
+    {
+        if (this.clickedTile != null && this.targetTile != null)
+        {
+            SwitchTiles(this.clickedTile, this.targetTile);
+        }
+    }
+
+    private void SwitchTiles(Tile clickedTile, Tile targetTile)
+    {
+        this.clickedTile = null;
+        this.targetTile = null;
     }
 }
