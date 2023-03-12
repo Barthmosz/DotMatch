58060051d7f0558ee2640b59092de50fe12bdb4b
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-12 20:29:33 -0300

    refactor: extract SwitchTiles logic to a coroutine

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -133,11 +133,21 @@ public class Board : MonoBehaviour
 
     private void SwitchTiles(Tile clickedTile, Tile targetTile)
     {
+        StartCoroutine(SwitchTilesRoutine(clickedTile, targetTile));
+    }
+
+    private IEnumerator SwitchTilesRoutine(Tile clickedTile, Tile targetTile)
+    {
         GamePiece clickedPiece = this.allGamePieces[clickedTile.xIndex, clickedTile.yIndex];
         GamePiece targetPiece = this.allGamePieces[targetTile.xIndex, targetTile.yIndex];
 
         clickedPiece.Move(targetTile.xIndex, targetTile.yIndex, this.swapTime);
         targetPiece.Move(clickedTile.xIndex, clickedTile.yIndex, this.swapTime);
+
+        yield return new WaitForSeconds(this.swapTime);
+
+        HighlightMatchesAt(clickedTile.xIndex, clickedTile.yIndex);
+        HighlightMatchesAt(targetTile.xIndex, targetTile.yIndex);
     }
 
     private bool IsNextTo(Tile start, Tile end)
