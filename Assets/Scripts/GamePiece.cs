85bd2f7a7a2fb19ac0a87077d344cf7a6c03fc12
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-15 23:55:09 -0300

    chore: remove unnecessary usings

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -1,12 +1,11 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GamePiece : MonoBehaviour
 {
     private Board board;
     private bool isMoving = false;
-  
+
     public int xIndex, yIndex;
 
     public enum InterpolationType
